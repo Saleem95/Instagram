7958893c01cd2df8383197e2237bb2c26acc6b1a
SALEEM <87938904+Saleem95@users.noreply.github.com>
2025-02-09 13:29:58 +0530

    Update story.spec.cy.ts

--- cypress/e2e/story.spec.cy.ts
+++ cypress/e2e/story.spec.cy.ts
@@ -1,6 +1,7 @@
 describe("Instagram Stories", () => {
   beforeEach(() => {
     cy.visit("http://localhost:5173"); // Change this URL if needed
+    // cy.visit("https://insta-ss.netlify.app/");   
   });
 
   it("should load stories", () => {
